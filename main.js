968e81e79909fc1c378963084e350f9249af2796
patricia <patriciapavia236@gmail.com>
2020-06-17 13:20:58 +1000

    add functionality and weather api to be able to display weather base on location

---
+++ main.js
@@ -0,0 +1,13 @@
+// // init weather object
+
+// const weather = new Weather("Melbourn", "Aus");
+// // Document.addEventListenner("DOMContentLoaded", runGetWeather);
+// runGetWeather();
+// function runGetWeather() {
+//   weather
+//     .getWeather()
+//     .then((results) => {
+//       Uint16Array.paint(results);
+//     })
+//     .catch((err) => console.log(err));
+// }
---
+++ weather.js
@@ -0,0 +1,23 @@
+async function getWeather(city, country) {
+  // await response of the fetch call
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=b739255c0b0dacd8806fb31e5da42c99`
+  );
+  // only proceed once its resolve
+  const data = await response.json();
+  // only proceed once second promise resolve
+  return data;
+}
+getWeather("London", "Uk").then(function (weather) {
+  const w = weather;
+  const location = weather.name;
+  const desc = weather.weather[0].description;
+  const icon = weather.weather[0].icon;
+  const wString = weather.main.temp;
+  console.log(icon);
+  console.log(w);
+  document.getElementById("w-location").innerHTML = location;
+  document.getElementById("w-desc").innerHTML = desc;
+  document.getElementById("w-icon").innerHTML = icon;
+  document.getElementById("w-string").innerHTML = wString;
+});
