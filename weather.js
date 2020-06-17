d563287403eda0e019d02686b2d686c0bb4f031e
patricia <patriciapavia236@gmail.com>
2020-06-17 15:47:40 +1000

    finished add functionality to change different city

--- weather.js
+++ weather.js
@@ -8,17 +8,18 @@ async function getWeather(city, country) {
   // only proceed once second promise resolve
   return data;
 }
-getWeather("Canberra", "Aus").then(function (weather) {
-  const w = weather;
+
+function loadWeather(weather) {
   const location = weather.name;
   const desc = weather.weather[0].description;
   const icon = weather.weather[0].icon;
-  const wString = `Today's Temp ${weather.main.temp} `;
+  const wString = `Today's Temp ${Math.round(weather.main.temp - 273.13)} C`;
   const humidnity = ` Humidnity:${weather.main.humidity}%`;
-  const feelLike = ` Feel Like:${weather.main.feels_like} `;
+  const feelLike = ` Feel Like:${Math.round(
+    weather.main.feels_like - 273.13
+  )} C`;
   const wind = `Wind Speed:${weather.wind.speed}, Wind Deg:${weather.wind.deg}`;
-  console.log(icon);
-  console.log(w);
+
   document.getElementById("w-location").innerHTML = location;
   document.getElementById("w-desc").innerHTML = desc;
   document.getElementById("w-icon").innerHTML = icon;
@@ -26,4 +27,19 @@ getWeather("Canberra", "Aus").then(function (weather) {
   document.getElementById("w-humidnity").innerHTML = humidnity;
   document.getElementById("w-feels-like").innerHTML = feelLike;
   document.getElementById("w-wind").innerHTML = wind;
-});
+}
+getWeather("Canberra", "Aus").then(loadWeather);
+
+document.getElementById("w-change-btn").addEventListener("click", changeCity);
+
+function changeCity(city, country) {
+  const cityValue = document.getElementById("city").value;
+  const countryValue = document.getElementById("country").value;
+  console.log(cityValue);
+  console.log(countryValue);
+  getWeather(`${cityValue}`, `${countryValue}`).then(loadWeather);
+
+  // close modal
+
+  $("#locModal").modal("hide");
+}
